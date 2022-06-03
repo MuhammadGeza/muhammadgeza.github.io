23a3736c0e661301d8eacad270db27c6568c101d
Muhammad Geza <gezaruly12@gmail.com>
2022-06-03 10:43:27 +0800

    Commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // Load More
 $('.single_post').slice(0, 3).show();
 $('.load').on('click', function () {
-  $('.single_post:hidden').slice(0, 3).slideDown();
+  $('.single_post:hidden').slice(0, 3).slideDown('slow');
   if ($('.single_post:hidden').length == 0) {
-    $('.load').fadeOut('slow');
+    $('.load').fadeOut('');
   }
 });
 //  End Load More
